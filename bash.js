b214bbadd311fdef8cdc7a071f639bb24b0c1c22
Dilan Ozusta <dilanozusta@outlook.com>
2018-09-18 11:50:22 -0500

    created ls and made it working on cat

--- bash.js
+++ bash.js
@@ -1,11 +1,17 @@
 process.stdout.write('prompt > ');
 
 const pwd = require('./pwd');
+const ls = require('./ls');
+const cat = require('./cat');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
   if (cmd === 'pwd') {
     pwd();
+  } else if (cmd === 'ls') {
+    ls();
+  } else if (cmd.slice(0,3) === 'cat') {
+    cat(cmd.slice(4));
   }
   process.stdout.write('You typed: ' + cmd);
   process.stdout.write('\nprompt > ');
