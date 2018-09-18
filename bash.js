1ba0b444b5b2fff22195ce7a24851f434caca6a2
Brett Scheller <brett.green.scheller@gmail.com>
2018-09-18 11:27:11 -0500

    finished pwd func

---
+++ bash.js
@@ -0,0 +1,12 @@
+process.stdout.write('prompt > ');
+
+const pwd = require('./pwd');
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+  if (cmd === 'pwd') {
+    pwd();
+  }
+  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write('\nprompt > ');
+});
